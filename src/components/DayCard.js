20f239189cb633e73b1091934a325fc3e0e6ebd1
drew-kd-rodgers <andrew.k.d.rodgers@gmail.com>
2025-05-14 16:17:48 +0100

    Add weather names

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -52,6 +52,64 @@ function getWeatherIcon(code, night) {
 }
 
 function getWeatherName(code) {
+    switch (code) {
+        case 0:
+            return "Clear Sky";
+        case 1:
+            return "Lightly Cloudy";
+        case 2:
+            return "Partly Cloudy";
+        case 3:
+            return "Overcast";
+        case 45:
+            return "Fog";
+        case 48:
+            return "Rime Fog";
+        case 51:
+            return "Light Drizzle";
+        case 53:
+            return "Moderate Drizzle";
+        case 55:
+            return "Dense Drizzle";
+        case 56:
+            return "Light Freezing Drizzle";
+        case 57:
+            return "Dense Freezing Drizzle";
+        case 61:
+            return "Light Rain";
+        case 63:
+            return "Moderate Rain";
+        case 65:
+            return "Heavy Rain";
+        case 66:
+            return "Light Freezing Rain";
+        case 67:
+            return "Heavy Freezing Rain";
+        case 71:
+            return "Light Snowfall";
+        case 73:
+            return "Moderate Snowfall";
+        case 75:
+            return "Heavy Snowfall";
+        case 77:
+            return "Snow Grains";
+        case 80:
+            return "Light Showers";
+        case 81:
+            return "Moderate Showers";
+        case 82:
+            return "Heavy Showers";
+        case 85:
+            return "Light Snow Showers";
+        case 86:
+            return "Heavy Snow Showers";
+        case 95:
+            return "Thunderstorm";
+        case 96:
+            return "Thunderstorm with Slight Hail";
+        case 99:
+            return "Thunderstorm with Heavy Hail";
+    }
     return "Weather undefined";
 }
 
