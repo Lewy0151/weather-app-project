bab1bfc3f1d637d0c2445d216d437e5bbbb2551e
Lewy0151 <lewiscargill@hotmail.com>
2025-05-15 12:43:03 +0100

    added font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito_Sans } from 'next/font/google';
 import "./globals.css";
+
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'], 
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunitoSans.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
